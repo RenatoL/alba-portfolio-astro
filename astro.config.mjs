4a86b07b22040f37622394b478408af932ce3e4d
Renato Lourinho <lourinho.renato@gmail.com>
2024-12-13 18:46:13 +0000

    chore: upgrade to Astro v5
    breaking changes: https://docs.astro.build/zh-cn/guides/upgrade-to/v5/#changed-image-endpoint-configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+defineConfig({
+    image: {
+      endpoint: {
+        route: "/image",
+        entrypoint: "./src/image_endpoint.ts"
+      }
+    },
+  })
+
 // https://astro.build/config
 export default defineConfig({});
+
+
